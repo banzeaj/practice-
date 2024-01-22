f06de183b9fffca64ac289ee26c911f79106d230
banzeaj <banze.aj@gmail.com>
2024-01-22 15:16:36 +0200

    other day other dollaaa

--- pratice.js
+++ pratice.js
@@ -15,3 +15,29 @@ a =7;
 b = a;
 
 console.log(a);
+
+//initialize 
+var a = 5;
+var b =5 ;
+var c =0;
+
+c=a+b;
+console.log(c);
+
+/********
+ 
+ code  output 
+ \'    single quote
+ \"    double quote
+ \\    backslash
+ \n    newline 
+ 
+ *******/
+
+ // concatenation +
+
+ var status1 = "my name is "+ myNume + " im this old "+ a;
+
+ console.log(status1);
+
+
