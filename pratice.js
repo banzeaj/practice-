d3ee7206fc77a4e719c3f789ae6e1c83134dc0c5
banzeaj <banze.aj@gmail.com>
2024-01-23 10:59:04 +0200

    as we try again

--- pratice.js
+++ pratice.js
@@ -41,3 +41,46 @@ console.log(c);
  console.log(status1);
 
 
+// start at 30minutes 
+//methods
+
+
+// finding the length of a word 
+
+var lastNameLength = 0;
+var lastName = "antonio";
+
+lastNameLength =lastName.length;
+
+console.log(lastNameLength);
+
+var lastLetterofName =" ";
+var LetterofName;
+
+LetterofName = lastName[0]; // taking out letters from words 
+lastLetterofName = lastName[lastNameLength -1];// gitting the last letter
+
+console.log(LetterofName);
+console.log(lastLetterofName); 
+
+//array 
+
+var ourArray = ["joni", 22];
+var arrayone= [22,55,44,66,77];
+var dataarry = arrayone[2];
+
+var yourarray =[[1,2,2,1],[5,6,2,2],[1,5,6,8]];
+arrayone[2] =50; // updating an array
+
+
+console.log(dataarry);
+console.log(arrayone[2]);
+console.log(yourarray[2][1]);
+
+// function
+
+function ourReusableFunction(){
+console.log("lol this is happening "); // creating a function 
+
+}
+ourReusableFunction(); //this is how we call a funtion 
