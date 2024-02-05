4d30b689da26e806966c9d0b60d190da73a530b0
banzeaj <banze.aj@gmail.com>
2024-02-05 21:30:38 +0200

    hear we go

---
+++ loops.js
@@ -0,0 +1,41 @@
+// snippet 1 
+
+console.log("hello");
+
+for (let i = 0; i <5; i++){
+
+    console.log("code");
+
+}
+
+console.log("goodbye");
+
+console.log("____________________________________________")
+//////////////////snippet 2 ////////////////////////////////
+
+console.log("hi");
+
+for(let i =3;i <=7; i++ ){
+
+    console.log("program");
+    console.log(i);
+
+}
+
+console.log("goodbye");
+
+console.log("-----------------------------------------------");
+
+let foo = function(){
+
+    for (let num = 10; num >0; num -=2){
+        console.log(num);
+    }
+}
+console.log("begin");
+foo();
+console.log("end");
+foo();
+
+
+////////////////////////////snippet 3/////////////////////////////////////
