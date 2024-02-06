985ecd3514f54a9d57c8bf6e77a2e129ac605eb4
banzeaj <banze.aj@gmail.com>
2024-02-06 09:22:15 +0200

    four

--- nested_loops.js
+++ nested_loops.js
@@ -30,3 +30,21 @@ for (let i = 0;i < locations.length; i++){
 console.log("--------------------------------------------------------------");
 
 let colors = ["red","purple","orange"];
+
+for (let i = 0; i <colors.length;i++){
+    let colorStr = colors[i];
+    console.log(colorStr);
+
+    for (let j =0; j < colorStr.length;j++){
+
+        let char = colorStr[j];
+        console.log(char);
+    }
+
+}
+}
+
+///////////////////////////////////////////////////////////////////////
+
+
+console.log("--------------------------------------------------------------");
